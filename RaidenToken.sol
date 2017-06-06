8cb8f040b48a1da912358fca051535d279e1b8b8
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2017-06-06 19:37:56 +0200

    linting and cleaning up RaidenToken.sol

--- RaidenToken.sol
+++ RaidenToken.sol
@@ -6,85 +6,83 @@ import "token.sol";
 
 contract RaidenToken is StandardToken {
 
-    /*
-     *  Token meta data
-     */
+    /*  Token meta data */
+
     string constant public name = "Raiden Token";
     string constant public symbol = "RDN";
     uint constant public decimals = 24;  // ETH has 18
-    uint public maxSupply = 10 * 1000000 * 10**decimals;
+    uint public maxSupply = 10 * 1000000 * 10**decimals; // TODO should be constant?
     address public mint;
+
+    /* Events */
 
     event Minted(address indexed receiver, uint num, uint _totalSupply);
     event Destroyed(address indexed receiver, uint num, uint _totalSupply);
     event CollateralUpdated(uint changed, uint newETHValue);
 
-    /*
-     *  Public functions
-     */
+    /*  Public functions */
+
     /// @dev Contract constructor function sets the mint contract address
     /// @param _mint Address of dutch auction contract.
     /// @param owners Array of addresses receiving preassigned tokens.
     /// @param numtokens Array of preassigned token amounts.
-    function RaidenToken(address _mint, address[] owners, uint[] numtokens)
-        public
-    {
-        // prealloc
-        for (uint i=0; i<owners.length; i++) {
-            assert(owners[i] != 0);
+    function RaidenToken(address _mint, address[] owners, uint[] numtokens) public {
+        // prealloc tokens
+        for (uint i = 0; i < owners.length; i++) {
+            require(owners[i] != 0x0);
             mint(owners[i], numtokens[i]);
         }
         assert(totalSupply <= maxSupply);
+
+        // assign mint
         mint = _mint;
     }
 
-    function mint(address receiver, uint num)
-        public
-        returns (bool)
-    {
-        assert(msg.sender == address(this) || msg.sender == mint);
+    function mint(address receiver, uint num) public returns (bool) {
+        require(msg.sender == address(this) || msg.sender == mint);
+
         totalSupply += num;
         assert(totalSupply <= maxSupply);
+
         balances[receiver] += num;
         Transfer(0, receiver, num);
+
         Minted(receiver, num, totalSupply);
         return true;
     }
 
     // used to safely send ETH to the contract
     // FIXME: default function should fail, right?
-    function addCollateral()
-        public
-        payable
-        returns (bool)
-    {
-        assert(msg.sender == mint); // FIXME: required restriction?
+    function addCollateral() public payable returns (bool) {
+        require(msg.sender == mint); // FIXME: required restriction?
+
         CollateralUpdated(msg.value, this.balance);
         return true;
     }
 
-    function tokensPerWei()
-        public
-        constant
-        returns (uint)
-    {
+    function tokensPerWei() public constant returns (uint) {
         return totalSupply / this.balance; // wei per token FIXME
     }
 
-    function destroy(uint num)
-        public
-        returns (bool)
-    {
-        assert(balances[msg.sender] >= num);
-        balances[msg.sender] -= num;
-        totalSupply -= num;
-        maxSupply -= num;
+    function destroy(uint num) public returns (bool) {
+        require(balances[msg.sender] >= num);
+
         uint unlockedETH = num / tokensPerWei();
         uint pre = this.balance;
+
+        balances[msg.sender] -= num;
+        totalSupply -= num;
+        maxSupply -= num; // TODO should this happen? Or is this value constant?
+
+        // transfer unlocked ETH back to msg.sender
         msg.sender.transfer(unlockedETH);
-        assert(this.balance == pre - unlockedETH);
+
+        require(this.balance == pre - unlockedETH);
+
         Destroyed(msg.sender, num, totalSupply);
         CollateralUpdated(-unlockedETH, this.balance);
+        // TODO: maybe have separate event for increase and decrease collateral?
+
         return true;
     }
 }
