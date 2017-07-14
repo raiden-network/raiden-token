1872d8e3b0f882265ffacb57a547c8e343df09b8
Loredana <loredana.cirstea@gmail.com>
2017-07-14 10:00:31 +0300

    Update deployment script for local testrpc chain

--- deploy.py
+++ deploy.py
@@ -5,6 +5,11 @@ from populus import Project
 from populus.utils.wait import wait_for_transaction_receipt
 from web3 import Web3
 
+# base_price, price_factor, owner issuance fraction, owner issuance fraction decimals
+mint_args = [10**9, 15, 10, 2]
+
+# price_factor, price_const
+auction_args = [2 * 10**12, 10000]
 
 def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> dict:
 
@@ -24,11 +29,14 @@ def main():
     # We are working on a testnet
     chain_name = "ropsten"
     chain_name = "testrpc"
-    # chain_name = "tester"
+    chain_name = "testrpclocal"
 
     with project.get_chain(chain_name) as chain:
+        print('chain', chain)
+        # web3 = chain.web3
+        web3 = Web3
+        print("Web3 provider is", web3.currentProvider)
 
-        # Load Populus contract proxy classes
         Auction = chain.provider.get_contract_factory('Auction')
         Mint = chain.provider.get_contract_factory('Mint')
         Token = chain.provider.get_contract_factory('ContinuousToken')
@@ -41,14 +49,14 @@ def main():
         assert beneficiary, "Make sure your node has coinbase account created"
 
         # Deploy Auction
-        txhash = Auction.deploy(transaction={"from": beneficiary}, args=[10000, 100])
+        txhash = Auction.deploy(transaction={"from": beneficiary}, args=auction_args)
         print("Deploying auction, tx hash is", txhash)
         receipt = check_succesful_tx(web3, txhash)
         auction_address = receipt["contractAddress"]
         print("Auction contract address is", auction_address)
 
         # Deploy Mint
-        txhash = Mint.deploy(transaction={"from": beneficiary}, args=[100, 15, 10, 2])
+        txhash = Mint.deploy(transaction={"from": beneficiary}, args=mint_args)
         print("Deploying mint, tx hash is", txhash)
         receipt = check_succesful_tx(web3, txhash)
         mint_address = receipt["contractAddress"]
@@ -77,7 +85,6 @@ def main():
         print("Token total supply is", token.call().totalSupply())
         print("Mint total supply is", mint.call().issuedSupply())
         print("Auction price is", auction.call().price())
-
 
 if __name__ == "__main__":
     main()
