03db9812a2a497ff1663648420f5d0916e8df825
Loredana <loredana.cirstea@gmail.com>
2017-08-01 22:55:14 +0300

    Refactor token Python tests

--- tests_simple/test_fixtures.py
+++ tests_simple/test_fixtures.py
@@ -73,9 +73,9 @@ def accounts(web3):
     return get
 
 
-def create_contract(chain, contract_type, arguments):
-    print(chain, contract_type, arguments)
-    deploy_txn_hash = contract_type.deploy(args=arguments)
+def create_contract(chain, contract_type, arguments, transaction=None):
+    print(chain, contract_type, arguments, transaction)
+    deploy_txn_hash = contract_type.deploy(transaction=transaction, args=arguments)
     contract_address = chain.wait.for_contract_address(deploy_txn_hash)
     contract = contract_type(address=contract_address)
     return contract
