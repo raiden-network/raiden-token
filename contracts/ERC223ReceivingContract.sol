0a6023519d48d63f008b01440324e8c57d7efb91
Loredana <loredana.cirstea@gmail.com>
2017-09-07 20:03:29 +0300

    Add support for `tokenFallback` (ERC223)
    Issue https://github.com/raiden-network/raiden-token/issues/25
    ERC223: https://github.com/ethereum/EIPs/issues/223

---
+++ contracts/ERC223ReceivingContract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.11;
+
+ /*
+ * Contract that is working with ERC223 tokens
+ */
+
+/// @title ERC223ReceivingContract - Standard contract implementation for compatibility with ERC223 tokens.
+contract ERC223ReceivingContract {
+
+    /// @dev Function that is called when a user or another contract wants to transfer funds.
+    /// @param _from Transaction initiator, analogue of msg.sender
+    /// @param _value Number of tokens to transfer.
+    /// @param _data Data containig a function signature and/or parameters
+    function tokenFallback(address _from, uint256 _value, bytes _data);
+}
