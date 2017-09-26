06f600d84dbde87e23eed967e682fb4ef343b886
Loredana <loredana.cirstea@gmail.com>
2017-09-26 21:32:17 +0300

    Handle `approve` race condition
    Set allowance to 0 before changing the approved amount.
    https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md#approve

--- contracts/token.sol
+++ contracts/token.sol
@@ -143,7 +143,12 @@ contract StandardToken is Token {
     /// @return Returns success of function call.
     function approve(address _spender, uint256 _value) public returns (bool) {
         require(_spender != 0x0);
-        require(_value > 0);
+
+        // To change the approve amount you first have to reduce the addresses`
+        // allowance to zero by calling `approve(_spender,0)` if it is not
+        // already 0 to mitigate the race condition described here:
+        // https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
 
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
