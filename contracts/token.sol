58e322a6aa64ae340021625ad83a781ce5c23050
Loredana <loredana.cirstea@gmail.com>
2017-09-27 16:21:27 +0300

    Minor changes post-review

--- contracts/token.sol
+++ contracts/token.sol
@@ -145,10 +145,10 @@ contract StandardToken is Token {
         require(_spender != 0x0);
 
         // To change the approve amount you first have to reduce the addresses`
-        // allowance to zero by calling `approve(_spender,0)` if it is not
+        // allowance to zero by calling `approve(_spender, 0)` if it is not
         // already 0 to mitigate the race condition described here:
         // https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+        require(_value == 0 || allowed[msg.sender][_spender] == 0);
 
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
