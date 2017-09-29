ae39357c3c5d871c693f2ef5c43d63cda2985e74
Jozef Henzl <popcorp86@gmail.com>
2017-09-29 11:58:52 +0200

    sampler: added additional items to /status endpoint

--- event_sampler/resources.py
+++ event_sampler/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import logging
 import numpy
 
@@ -27,31 +27,33 @@ log = logging.getLogger(__name__)
 # }
 
 
-class LastBidSubmission(Resource):
-    def __init__(self, sampler):
-        super(LastBidSubmission, self).__init__()
-        self.sampler = sampler
-
-    def get(self):
-        return self.sampler.last_event()
-
-
 class BidsHistogram(Resource):
     def __init__(self, sampler):
         super(BidsHistogram, self).__init__()
         self.sampler = sampler
 
     def get(self):
+        if len(self.sampler.events.items()) == 0:
+            return "No events", 503
+        parser = reqparse.RequestParser()
+        parser.add_argument('bins', help='bins in the histogram', default=20, type=int)
+        args = parser.parse_args()
         block_to_events = {}
         for block, events in self.sampler.events.items():
             block_to_events[block] = sum((event['args']['_amount'] for event in events))
         min_block = min(block_to_events.keys())
         max_block = max(block_to_events.keys())
-        bins = range(min_block, max_block, ((max_block - min_block) // 10))
+        bins = range(min_block, max_block, ((max_block - min_block) // args['bins']))
         ar, ar_bins = numpy.histogram(list(block_to_events.keys()),
                                       bins=bins,
                                       weights=list(block_to_events.values()))
-        return {'y': ar.tolist(), 'x': ar_bins.tolist()}
+        web3 = self.sampler.chain.web3
+        bin_timestamps = [web3.eth.getBlock(block_id)['timestamp']
+                          for block_id in ar_bins.tolist()]
+        return {'timestamped_bins': bin_timestamps,
+                'block_bins': bin_timestamps,
+                'bin_sum': ar.tolist(),
+                'bin_cumulative_sum': numpy.cumsum(ar).tolist()}
 
 
 class AuctionStatus(Resource):
@@ -61,6 +63,8 @@ class AuctionStatus(Resource):
         self.sampler = sampler
 
     def get(self):
+        last_event = self.sampler.last_event()
+        web3 = self.sampler.chain.web3
         ret = {}
         ret['auction_stage'] = self.contract.call().stage()
         block_to_sum = {}
@@ -68,4 +72,15 @@ class AuctionStatus(Resource):
             block_to_sum[block] = sum((event['args']['_amount'] for event in events))
 
         ret['raised_eth'] = sum((v for v in block_to_sum.values()))
+        ret['final_price'] = self.sampler.final_price
+        ret['claimed_tokens'] = self.sampler.total_claimed
+        if last_event is not None:
+            ret['timestamp'] = web3.eth.getBlock(last_event['blockNumber'])['timestamp']
+        else:
+            ret['timestamp'] = None
+        ret['start_time'] = self.sampler.auction_start_time
+        ret['end_time'] = self.sampler.auction_end_time
+        ret['price_start'] = self.sampler.price_start
+        ret['price_constant'] = self.sampler.price_constant
+        ret['price_exponent'] = self.sampler.price_exponent
         return ret
