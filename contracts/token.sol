59c0f60e3524a9eb24741f581fb3abec389982bc
Lefteris Karapetsas <lefteris@refu.co>
2017-09-29 11:23:39 +0200

    Get rid of solc v0.4.17 warnings

--- contracts/token.sol
+++ contracts/token.sol
@@ -28,16 +28,16 @@ contract Token {
     /*
      * ERC 20
      */
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 _value) returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    function balanceOf(address _owner) public constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
 
     /*
      * ERC 223
      */
-    function transfer(address _to, uint256 _value, bytes _data) returns (bool success);
+    function transfer(address _to, uint256 _value, bytes _data) public returns (bool success);
 
     /*
      * Events
